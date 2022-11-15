397c4801a2da17e6dd47376baaaf514fa3bcdeaf
Max Waring <max.waring@adalpha.io>
2022-11-15 16:28:58 +0000

    Create a window for top-down and 3D rendering

--- src/main.c
+++ src/main.c
@@ -24,9 +24,15 @@ int main(int argc, char* args[]) {
 		printf("Error initializing SDL: %s\n", SDL_GetError());
 	}
 
-	SDL_Window *window = SDL_CreateWindow("Raycasting", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREENWIDTH, SCREENHEIGHT, 0);
 	Uint32 renderFlags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, renderFlags);
+
+	// Create the window for the top-down view
+	SDL_Window *topDownWindow = SDL_CreateWindow("Raycasting", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREENWIDTH, SCREENHEIGHT, 0);
+	SDL_Renderer *topDownRenderer = SDL_CreateRenderer(topDownWindow, -1, renderFlags);
+
+	// Create the window for the 3D rendered view
+	SDL_Window *window3D = SDL_CreateWindow("Raycasting", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREENWIDTH, SCREENHEIGHT, 0);
+	SDL_Renderer *window3DRenderer = SDL_CreateRenderer(window3D, -1, renderFlags);
 
 	Source lightSource;
 	Vec2 srcPos = {SCREENWIDTH / 2, SCREENHEIGHT / 2};
@@ -45,31 +51,37 @@ int main(int argc, char* args[]) {
 		SDL_GetMouseState(&mousex, &mousey);
 		moveSource(&lightSource, (Vec2) {mousex, mousey});
 
-		// Clear the screen
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-		SDL_RenderClear(renderer);
+		// Clear the screens
+		SDL_SetRenderDrawColor(topDownRenderer, 0, 0, 0, 0);
+		SDL_RenderClear(topDownRenderer);
+		SDL_SetRenderDrawColor(window3DRenderer, 255, 0, 0, 0);
+		SDL_RenderClear(window3DRenderer);
 
 		// Draw the light rays
-		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+		SDL_SetRenderDrawColor(topDownRenderer, 255, 255, 255, 0);
 		for (int i=0; i<NUM_RAYS; i++) {
 			Vec2 rayEnd = cast(&lightSource.rays[i], bounds, NUM_BOUNDS);
 			//printf("ray end: %f, %f\n", rayEnd.x, rayEnd.y);
 			if (rayEnd.x != -1 && rayEnd.y != -1) {
-				SDL_RenderDrawLineF(renderer, 
+				SDL_RenderDrawLineF(topDownRenderer, 
 								    lightSource.rays[i].pos.x, lightSource.rays[i].pos.y,
 									rayEnd.x, rayEnd.y);
 			}
 		}
 		for (int i=0; i<NUM_BOUNDS; i++) {
-			SDL_RenderDrawLineF(renderer, bounds[i].start.x, bounds[i].start.y,
+			SDL_RenderDrawLineF(topDownRenderer, bounds[i].start.x, bounds[i].start.y,
 								bounds[i].end.x, bounds[i].end.y);
 		}
 
-		SDL_RenderPresent(renderer);
+		SDL_RenderPresent(topDownRenderer);
+		SDL_RenderPresent(window3DRenderer);
 	}
 	
 	// Clean up and quit
-	SDL_DestroyWindow(window);
+	SDL_DestroyWindow(topDownWindow);
+	SDL_DestroyWindow(window3D);
+	SDL_DestroyRenderer(topDownRenderer);
+	SDL_DestroyRenderer(window3DRenderer);
 	SDL_Quit();
 	return 0;
 }
